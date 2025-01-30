d33be04e7abbbd97a3c0e675a1c224b1a54d5e6d
Ege Yiğit Çelik <yigit.celik@hotmail.com>
2025-01-30 11:28:13 +0300

    Added some unused changes to create data for PMI code and added 512 token limit

--- scripts/pretraining_create_data_for_PMI.py
+++ scripts/pretraining_create_data_for_PMI.py
@@ -9,10 +9,11 @@ import torch
 import math
 #  import re
 import tqdm
+import gc
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--c4_split", type=str, default="realnewslike", choices=["en","realnewslike"])
+parser.add_argument("--c4_split", type=str, default="realnewslike", choices=["en", "realnewslike"])
 #  parser.add_argument("--rouge_type", type=str, default="rouge1", choices=["rouge1","rouge2","rougeL"])
 parser.add_argument("--topk", type=int, default=5)
 
@@ -32,25 +33,24 @@ model = T5ForConditionalGeneration.from_pretrained('t5-base')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 
-
 all_PMI_scores_dict = {}
 
 
 def conditional_probability(target_sentence, context_sentence):
     # Tokenize the context sentence
-    context_inputs = tokenizer(context_sentence, return_tensors='pt').to(device)
+    context_inputs = tokenizer(context_sentence, return_tensors='pt', truncation=True, max_length=512).to(device)
 
     # Tokenize the target sentence
-    target_inputs = tokenizer(target_sentence, return_tensors='pt').to(device)
+    target_inputs = tokenizer(target_sentence, return_tensors='pt', truncation=True, max_length=512).to(device)
 
     losses = model(context_inputs['input_ids'], labels=target_inputs['input_ids'])
-    #print("Probability of target sentence given context sentence:", losses.loss.item())
+    # print("Probability of target sentence given context sentence:", losses.loss.item())
     return math.exp(-losses.loss.item())
 
 
 def marginal_probability(context_sentence):
     # Tokenize the context sentence
-    context_inputs = tokenizer(context_sentence, return_tensors='pt').to(device)
+    context_inputs = tokenizer(context_sentence, return_tensors='pt', truncation=True, max_length=512).to(device)
 
     # Generate output
     output = model.generate(context_inputs['input_ids'], max_new_tokens=40, num_beams=5, no_repeat_ngram_size=2,
@@ -71,7 +71,20 @@ def calculate_pmi(target_sentence, doc_without_target_sentence):
     return pmi
 
 
+"""def reset_model(current_model):
+    # Clear the model
+    del current_model
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    # Reinitialize the model
+    model = T5ForConditionalGeneration.from_pretrained('t5-base')
+    model.to(device)
+    return model"""
+
+
 def calc_pmi_for_all(training_dataset):
+    global model
     for t in tqdm.tqdm(training_dataset):
         temp_text = t["text"]
         sentences = nltk.sent_tokenize(temp_text)
@@ -82,13 +95,14 @@ def calc_pmi_for_all(training_dataset):
             doc = " ".join([s for j, s in enumerate(sentences) if i != j])
             pmi_score = calculate_pmi(summ, doc)
             temp_scores.append(pmi_score)
-        all_PMI_scores_dict[temp_text] = temp_scores
+        all_PMI_scores_dict[temp_text] = temp_scores  # [:500]
+        #  model = reset_model(model)  # ****************************************************
 
 
 def calc_pmi_score_and_select_top_k(example):
     temp_text = example["text"]
     sentences = nltk.sent_tokenize(temp_text)
-    
+
     """scores = []
     for i, sent in enumerate(sentences):
         summ = sent
@@ -96,21 +110,22 @@ def calc_pmi_score_and_select_top_k(example):
         pmi_score = calculate_pmi(summ, doc)
         #  scores.append(pmi_score[args.rouge_type].fmeasure)
         scores.append(pmi_score)"""
-    scores = all_PMI_scores_dict[temp_text]
+    scores = all_PMI_scores_dict[temp_text]  # [:500]
 
     # top k
     if len(scores) <= args.topk:
         ind = np.arange(len(scores))
     else:
         ind = np.argpartition(scores, -args.topk)[-args.topk:]
-    
-    example["documents"] = [" ".join([s if j!=i else mask_token for j,s in enumerate(sentences)]) for i in ind ]
-    example["summaries"] = [ sentences[i] for i in ind ]
-    example["pmi"] = [ scores[i] for i in ind ]  #  ["rouge"]
+
+    example["documents"] = [" ".join([s if j != i else mask_token for j, s in enumerate(sentences)]) for i in ind]
+    example["summaries"] = [sentences[i] for i in ind]
+    example["pmi"] = [scores[i] for i in ind]  # ["rouge"]
 
     return example
 
-dataset = load_dataset("c4",args.c4_split, cache_dir="./cache")
+
+dataset = load_dataset("c4", args.c4_split, cache_dir="./cache")
 
 dataset.pop("validation")
 dataset["train"] = dataset["train"].select(list(range(1000)))
@@ -119,7 +134,7 @@ calc_pmi_for_all(dataset["train"])
 
 dataset["train"] = dataset["train"].map(
     calc_pmi_score_and_select_top_k,
-    remove_columns=["url","text","timestamp"],
+    remove_columns=["url", "text", "timestamp"],
     batched=False,
     num_proc=16,
     keep_in_memory=True
