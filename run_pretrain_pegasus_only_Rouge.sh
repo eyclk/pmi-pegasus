375eaba307350385734a6df2a5e4438670a217f7
Ege Yiğit Çelik <yigit.celik@hotmail.com>
2025-03-04 17:06:57 +0300

    Added code for basic rouge1 pretraining for 1 million subset

--- run_pretrain_pegasus_only_Rouge.sh
+++ run_pretrain_pegasus_only_Rouge.sh
@@ -3,7 +3,7 @@
 GPU_IDX=0
 
 deepspeed --include=localhost:"$GPU_IDX" src/main.py --fp16 \
---data_dir ./c4_realnewslike_processed_PMI_combined \
+--data_dir ./c4_realnewslike_processed_rouge_combined \
 --do_train --do_pretrain --model_name facebook/bart-base \
 --deepspeed src/ds_config.json \
 --per_device_train_batch_size 16 --gradient_accumulation_steps 2 \
@@ -11,4 +11,4 @@ deepspeed --include=localhost:"$GPU_IDX" src/main.py --fp16 \
 --logging_step 5000  --max_steps 500000 \
 --warmup_steps 20000 --save_steps 40000 \
 --max_source_length 512 --max_target_length 256 \
---output_dir ./models/pmi_pegasus_1_mil_subset_500000_steps --pretrain_model_type bart_base --tokenize_on_fly
+--output_dir ./models/rouge_pegasus_1_mil_subset_500000_steps --pretrain_model_type bart_base --tokenize_on_fly
