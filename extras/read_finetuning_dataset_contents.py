276dbc640712976f0d5a7501cc423458657cfe45
Ege Yiğit Çelik <yigit.celik@hotmail.com>
2025-03-29 22:17:56 +0300

    Added corrector_no_shards for cnn_dailymail

--- extras/read_finetuning_dataset_contents.py
+++ extras/read_finetuning_dataset_contents.py
@@ -1,13 +1,32 @@
 from datasets import Dataset
-import pandas as pd
+# import pandas as pd
 
 
-ds = Dataset.from_file("test_xsum/dataset.arrow")
+ds = Dataset.from_file("xsum_comb/train/dataset.arrow")  # cnn_comb/test/data-00000-of-00001.arrow
 
 pd_ds = ds.to_pandas()
 
 print(pd_ds.head(), "\n", pd_ds.columns, "\n", pd_ds.shape, "\n\n")
 
-# Index of observed example from the dataset
-index = 9
-print(pd_ds["document"][index], "\n", pd_ds["id"][index], "\n\n", pd_ds["summary"][index])
+# Search for a data storage area inside the dataset named "document_ents"
+# print(pd_ds["document"][1], "\n\n")  # ['document_ents']
+
+ds.features.output_all_columns = True
+print(ds.format, "\n", ds.features, "\n\n")  ## ====> Failed to find 'document_ents' in the dataset
+
+
+# Print first 5 observed examples from the dataset
+for i in range(5):
+    print(pd_ds["document"][i], "\n", pd_ds["id"][i], "\n\n", pd_ds["summary"][i], "\n\n")
+
+    print("====> document_ents: ", pd_ds["document_ents"][i], "\n\n")
+    print("====> summary_ents: ", pd_ds["summary_ents"][i], "\n\n")
+    print("-"*50, "\n\n")
+
+""" # Print the details of the example with the given 'summary', e.g., On the first day in his new job, Choe Peng Sum was given a fairly simple brief: "Just go make us a lot of money."
+selected_example = pd_ds[pd_ds["summary"] == "US tennis star Venus Williams has been involved in a car accident that led to the death of a 78-year-old man."]
+
+# Get index of selected_example
+selected_example_index = selected_example.index[0]
+
+print(pd_ds["document"][selected_example_index], "\n", pd_ds["id"][selected_example_index], "\n\n", pd_ds["summary"][selected_example_index])"""
--- scripts/create_dataset_for_cnn_dailymail.py
+++ scripts/create_dataset_for_cnn_dailymail.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
-import sys
+# import sys
 
 dataset_name = ""
 
@@ -17,4 +17,19 @@ else:
 
     print(dataset.column_names)
 
-dataset.save_to_disk("cnn_dailymail_raw")
+# dataset.save_to_disk("cnn_dailymail_raw")
+
+dataset["train"].save_to_disk("cnn_dailymail_raw/train")
+dataset["validation"].save_to_disk("cnn_dailymail_raw/validation")
+dataset["test"].save_to_disk("cnn_dailymail_raw/test")
+
+
+"""dataset = load_dataset("EdinburghNLP/xsum")
+
+print(dataset.column_names)
+
+# Search for a column or a row named 'length' in the dataset
+if "length" in dataset.column_names:
+    print(dataset["length"])
+
+dataset.save_to_disk("xsum_raw")"""
