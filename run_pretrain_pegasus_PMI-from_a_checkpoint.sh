08ad23f492e8685b05dee380259a8aa5a42d0394
Ege Yiğit Çelik <47188625+eyclk@users.noreply.github.com>
2026-04-10 13:45:11 +0300

    Added additional helper script

---
+++ run_pretrain_pegasus_PMI-from_a_checkpoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+GPU_IDX=0
+
+deepspeed --include=localhost:"$GPU_IDX" src/main.py --fp16 \
+--data_dir ./PREPROCESSED_DATASETS/COMPLETE_c4_realnewslike_processed_PMI \
+--do_train --do_pretrain --model_name facebook/bart-base --resume_from_checkpoint ./models/PMI_pegasus__complete_realnewslike_2_MIL_steps/checkpoint-1500000 \
+--deepspeed src/ds_config.json \
+--per_device_train_batch_size 16 --gradient_accumulation_steps 2 \
+--learning_rate 1e-4 --weight_decay 0.01 \
+--logging_step 20000  --max_steps 3000000 \
+--warmup_steps 20000 --save_steps 500000 \
+--max_source_length 512 --max_target_length 256 \
+--output_dir ./models/PMI_pegasus__complete_realnewslike_3_MIL_steps --pretrain_model_type bart_base --tokenize_on_fly
+
+
